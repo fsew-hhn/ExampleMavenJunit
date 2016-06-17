94f936baceac7eef8e8e5fe4135cd8ddb0061fe8
bstecher <meisterchin25@web.de>
2016-06-17 12:06:30 +0200

    Full project - initial

---
+++ src/main/java/ExampleMavenJunit/ExampleMavenJunitArtifactID/bankaccount.java
@@ -0,0 +1,19 @@
+package ExampleMavenJunit.ExampleMavenJunitArtifactID;
+
+public class bankaccount {
+	private double balance = 0;
+	
+	public void deposit(double x) { 
+		balance += x;
+	}
+	
+	public void withdraw(double x) {
+		if(x > balance){
+			throw new RuntimeException("geht nicht");
+		}
+		balance -= x;
+	}
+	public double getBalance() {
+		return balance;
+	}
+}
---
+++ src/test/java/ExampleMavenJunit/ExampleMavenJunitArtifactID/bankaccountTest.java
@@ -0,0 +1,41 @@
+package ExampleMavenJunit.ExampleMavenJunitArtifactID;
+
+import ExampleMavenJunit.ExampleMavenJunitArtifactID.bankaccount;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+/*import org.junit.Ignore;*/
+import org.junit.Test;
+
+import ExampleMavenJunit.ExampleMavenJunitArtifactID.bankaccount;
+
+/*import org.junit.Test;*/
+
+public class bankaccountTest {
+	private bankaccount account;
+	@Test
+	public void testDeposit() {
+		account.deposit(100);
+		assertTrue("Test Deposit", 1100 == account.getBalance());
+	}
+	@Before
+	public void init() {
+		account = new bankaccount();
+		account.deposit(1000);
+	}
+	@Test 
+	public void testWithdraw() {
+		account.withdraw(500);
+		assertTrue("Test Withdraw", 500 == account.getBalance());
+	}
+	@Test(expected=RuntimeException.class)
+	public void testNegativeWithdraw() throws Exception{
+		account.withdraw(20000);
+	}
+	@Test(timeout=1000)
+	public void timeout() throws Exception{
+		Thread.sleep(2000);
+		account.deposit(2000);
+	}
+}
